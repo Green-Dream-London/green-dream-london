0ae6bc4fae419e860c3835363c20c3ab9b41793f
Lyubo Balev <lyubobalev@Lyubos-MacBook-Pro.local>
2024-06-18 19:39:36 +0300

    Mac acceptable updates

--- utils/send-email.js
+++ utils/send-email.js
@@ -1,43 +1,50 @@
-const nodemailer = require('nodemailer')
-const MailMessage = require('nodemailer/lib/mailer/mail-message')
-const constants = require('../common/email-constants')
-const logger = require('../utils/logger')
-const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer');
+const constants = require('../common/email-constants');
+const logger = require('../utils/logger');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
 
 const sendEmail = async (email) => {
-    try {
-        return await sgMail.send({
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: constants.subject,
-            html: constants.html
-        })
-    }   
-    catch (err) {
-        logger.error(err.toString())
-    }
-}
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: constants.subject,
+      html: constants.html
+    };
+    return await transporter.sendMail(mailOptions);
+  } catch (err) {
+    logger.error('send' + err.toString());
+  }
+};
 
 const receiveEmail = async (subject, text, attachments) => {
-    try {
-        return await sgMail.send({
-            from: process.env.EMAIL_USER,
-            to: process.env.EMAIL_USER,
-            subject,
-            text,
-            attachments
-        }) 
-    }   
-    catch (err) {
-        logger.error(err.toString())
-    }
-}
+  try {
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      subject,
+      text,
+      attachments: attachments.map(attachment => ({
+        filename: attachment.filename,
+        path: attachment.path
+      }))
+    };
+    return await transporter.sendMail(mailOptions);
+  } catch (err) {
+    logger.error(err.toString());
+  }
+};
 
 module.exports = {
-    sendEmail,
-    receiveEmail
-}
+  sendEmail,
+  receiveEmail
+};
